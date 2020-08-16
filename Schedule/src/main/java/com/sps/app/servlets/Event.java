359cba921967f03638b50a67d0a9a374d5d3d490
teefang <teefang@umich.edu>
2020-08-16 21:19:56 +0800

    Add doDelete and fix doGet Date format

--- Schedule/src/main/java/com/sps/app/servlets/Event.java
+++ Schedule/src/main/java/com/sps/app/servlets/Event.java
@@ -10,8 +10,8 @@ import java.text.*;
 public class Event {
     private String userId;
     private String title;
-    private Date startTime;
-    private Date endTime;
+    private String startTime;
+    private String endTime;
     private String content;
 
   /**
@@ -21,7 +21,14 @@ public class Event {
    * @param timestamp The time when the event takes place. Must be non-null.
    * @param content The task content. Can be null.
    */
-  public Event(String userId, String title, Date startTime, Date endTime, String content){
+//   public Event(String userId, String title, Date startTime, Date endTime, String content){
+//     this.startTime = startTime;
+//     this.endTime = endTime;
+//     this.userId = userId;
+//     this.title = title;
+//     this.content = content;
+//   }
+  public Event(String userId, String title, String startTime, String endTime, String content){
     this.startTime = startTime;
     this.endTime = endTime;
     this.userId = userId;
@@ -38,14 +45,14 @@ public class Event {
     return userId;
   }
   public String getStartTime() {
-    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");  
-    String strStartDate = formatter.format(this.startTime);
-    return strStartDate;
+    // SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");  
+    // String strStartDate = formatter.format(this.startTime);
+    return startTime;
   }
   public String getEndTime() {
-    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");  
-    String strEndDate = formatter.format(this.endTime);
-    return strEndDate;
+    // SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");  
+    // String strEndDate = formatter.format(this.endTime);
+    return endTime;
   }
   public String getContent() {
     return content;
--- Schedule/src/main/java/com/sps/app/servlets/EventServlet.java
+++ Schedule/src/main/java/com/sps/app/servlets/EventServlet.java
@@ -31,6 +31,9 @@ import com.google.appengine.api.datastore.KeyRange;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.datastore.Query.Filter; 
+import com.google.appengine.api.datastore.Query.FilterPredicate; 
+import com.google.appengine.api.datastore.Query.FilterOperator;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Map;
@@ -42,6 +45,7 @@ import java.lang.*;
 
 @WebServlet("/")
 public class EventServlet extends HttpServlet {
+  private DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -50,19 +54,20 @@ public class EventServlet extends HttpServlet {
     if(userId == null){
       response.setStatus(400);
       response.getWriter().println("userId cannot be empty");
+      return;
     }
 
     List<Event> eventList = new ArrayList<>();
     Query query = new Query("Event");
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     PreparedQuery results = datastore.prepare(query);
+    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH);
 
     for (Entity entity : results.asIterable()) {
-      response.setStatus(400);
-      if((String) entity.getProperty("userId") == userId){
+      String entityUser = (String) entity.getProperty("userId");
+      if(entityUser.equals(userId)){
         String title = (String) entity.getProperty("title");
-        Date startTime = (Date) entity.getProperty("startTime");
-        Date endTime = (Date) entity.getProperty("endTime");
+        String startTime = (String)entity.getProperty("startTime");
+        String endTime = (String)entity.getProperty("endTime");
         String content = (String) entity.getProperty("content");
 
         Event event = new Event(userId, title, startTime, endTime, content);
@@ -71,7 +76,7 @@ public class EventServlet extends HttpServlet {
     }
 
     String json = convertToJsonUsingGson(eventList);
-    response.setContentType("application/json;");
+    response.setContentType("application/json; charset=UTF-8");
     response.getWriter().println(json);
   }
     
@@ -90,17 +95,20 @@ public class EventServlet extends HttpServlet {
     String endTimeString = getParameter(request, "endTime", "");
     String content = getParameter(request, "content", "");
     
-    if (userId == null) {
+    if (userId == "") {
       response.setStatus(400);
       response.getWriter().println("userId cannot be null");
+      return;
     }
-    else if (title == null) {
+    else if (title == "") {
       response.setStatus(400);
       response.getWriter().println("title cannot be null");
+      return;
     }
-    else if (startTimeString == null || endTimeString == null){
+    else if (startTimeString == "" || endTimeString == ""){
       response.setStatus(406);
       response.getWriter().println("Time cannot be null");
+      return;
     }
 
     SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH);
@@ -113,15 +121,17 @@ public class EventServlet extends HttpServlet {
     catch(Exception e){
       response.setStatus(406);
       response.getWriter().println("Date format error");
+      return;
     }
 
     String id = "";
     if (startTime.after(endTime)) {
       response.setStatus(406);
       response.getWriter().println("endTime cannot be before startTime");
+      return;
     }
     else{
-      Event event = new Event(userId, title, startTime, endTime, content);
+      Event event = new Event(userId, title, startTimeString, endTimeString, content);
 
       Entity eventEntity = new Entity("Event");
       eventEntity.setProperty("userId", event.getuserId());
@@ -130,28 +140,43 @@ public class EventServlet extends HttpServlet {
       eventEntity.setProperty("endTime", event.getEndTime());
       eventEntity.setProperty("content", event.getContent());
 
-      DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
       datastore.put(eventEntity);
       id = String.valueOf(eventEntity.getKey().getId());
 
       Gson gson = new Gson();
       String retJson = gson.toJson(id);
 
-      response.setContentType("application/json;");
+      response.setContentType("application/json; charset=UTF-8");
       response.getWriter().println(retJson);
     }
   }
   
   @Override
   public void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     String userId = getParameter(request, "userId", "");
-    int eventId = Integer.parseInt(getParameter(request, "eventId", ""));
+    Long eventId = Long.parseLong(request.getParameter("eventId"));
     
-    Key eventKey = KeyFactory.createKey("Event", eventId);
-    datastore.delete(eventKey);
+    response.setContentType("application/json; charset=UTF-8");
 
+    Filter userIdFilter = new Query.FilterPredicate ("userId", Query.FilterOperator.EQUAL, userId);
+    Query query = new Query("Event").setFilter(userIdFilter);
+    PreparedQuery results = datastore.prepare(query);
+    Key eventKey = null;
 
+    for (Entity entity : results.asIterable()) {
+      response.getWriter().println(entity.getKey().getId());
+      if(eventId == entity.getKey().getId()){
+        eventKey = entity.getKey();
+      }
+    }
+    if(eventKey == null){
+      response.getWriter().println("no eventId exists");
+      response.setStatus(400);
+    }
+    else{
+        datastore.delete(eventKey);
+        response.setStatus(204);
+    } 
   }
   private String getParameter(HttpServletRequest request, String name, String defaultValue) {
     String value = request.getParameter(name);
