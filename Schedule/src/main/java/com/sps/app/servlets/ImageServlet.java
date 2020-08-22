21f5d0e4f41ed203d69a8a32c5a3e63d84a89623
teefang <teefang@umich.edu>
2020-08-22 13:23:15 +0000

    Add Blobstore servlet and Image servlet

---
+++ Schedule/src/main/java/com/sps/app/servlets/ImageServlet.java
@@ -0,0 +1,95 @@
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
+import com.google.gson.Gson;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.KeyRange;
+import com.google.appengine.api.blobstore.BlobInfo;
+import com.google.appengine.api.blobstore.BlobInfoFactory;
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.datastore.Query.Filter; 
+import com.google.appengine.api.datastore.Query.FilterPredicate; 
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.images.ImagesService;
+import com.google.appengine.api.images.ImagesServiceFactory;
+import com.google.appengine.api.images.ServingUrlOptions;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+import com.sps.app.Event;
+
+@WebServlet("/image")
+public class ImageServlet extends HttpServlet {
+  private DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String eventId = request.getParameter("eventId");
+    String imgUrl = getUploadedFileUrl(request, "imgUrl");
+    if(imgUrl == null){
+      response.getWriter().println("no file uploaded");
+      response.setStatus(400);
+    }
+
+    Key eventKey = KeyFactory.createKey("Event", Long.parseLong(eventId));
+    if(eventKey == null){
+      response.getWriter().println("eventId does not exist");
+      response.setStatus(406);
+    }
+
+    Entity eventEntity = null;
+    try{
+      eventEntity = datastore.get(eventKey);
+    }
+    catch(Exception e){
+      response.getWriter().println("event does not exist");
+      response.setStatus(406);
+    }
+
+    eventEntity.setProperty("imgUrl", imgUrl);
+    datastore.put(eventEntity);
+  }
+
+  /** Returns a URL that points to the uploaded file, or null if the user didn't upload a file. */
+  private String getUploadedFileUrl(HttpServletRequest request, String formInputElementName) {
+    BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+    Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(request);
+    List<BlobKey> blobKeys = blobs.get(formInputElementName);
+
+    // User submitted form without selecting a file, so we can't get a URL. (dev server)
+    if (blobKeys == null || blobKeys.isEmpty()) {
+      return null;
+    }
+
+    // Our form only contains a single file input, so get the first index.
+    BlobKey blobKey = blobKeys.get(0);
+
+    // User submitted form without selecting a file, so we can't get a URL. (live server)
+    BlobInfo blobInfo = new BlobInfoFactory().loadBlobInfo(blobKey);
+    if (blobInfo.getSize() == 0) {
+      blobstoreService.delete(blobKey);
+      return null;
+    }
+
+    // Use ImagesService to get a URL that points to the uploaded file.
+    ImagesService imagesService = ImagesServiceFactory.getImagesService();
+    ServingUrlOptions options = ServingUrlOptions.Builder.withBlobKey(blobKey);
+    String url = imagesService.getServingUrl(options);
+
+    return url;
+  }
+
+}
